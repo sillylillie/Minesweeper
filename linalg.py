e143be7c56e67aa3afcd76801a7552e278366bd1
Lillie Mucha <lillie.mucha@wipro.com>
2020-03-17 21:04:12 -0400

    Add linear algebra solutions file

---
+++ linalg.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def solution(solver, turn):
+	print('solver: {}'.format(solver))
+	print('bombs: {}'.format(turn['BOMBS']))
+	print('time: {}'.format(turn['TIME']))
+	# print('board: \n{}'.format(np.mat(turn['BOARD'])))
+
+	return [],[]
